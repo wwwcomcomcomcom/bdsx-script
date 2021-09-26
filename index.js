99c059d864616384d77e6494fcd814a6ab4bafb1
wwwcomcomcomcom <68013923+wwwcomcomcomcom@users.noreply.github.com>
2021-09-26 21:05:59 +0900

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./chat");
-require("./command");
-require("./packetsTest");
-console.log("Chat Loaded");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLGtCQUFnQjtBQUNoQixxQkFBbUI7QUFDbkIseUJBQXVCO0FBQ3ZCLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxDQUFDLENBQUMifQ==
